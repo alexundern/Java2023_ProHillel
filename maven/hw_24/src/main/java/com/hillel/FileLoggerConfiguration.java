40cff065ff0d49fbaeda05dff586d857e8ee382b
Alexander <novicov7@gmail.com>
2023-07-21 17:09:13 +0300

    Completed homework #24

--- maven/hw_24/src/main/java/com/hillel/FileLoggerConfiguration.java
+++ maven/hw_24/src/main/java/com/hillel/FileLoggerConfiguration.java
@@ -1,31 +1,13 @@
 package com.hillel;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class FileLoggerConfiguration {
     private final String path; // ../result.log
     private final Level level;
     private final long maxFileSize; // in bytes
     private final String logFormat;
-
-    public FileLoggerConfiguration(String path, Level level, long maxFileSize, String logFormat) {
-        this.path = path;
-        this.level = level;
-        this.maxFileSize = maxFileSize;
-        this.logFormat = logFormat;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public long getMaxFileSize() {
-        return maxFileSize;
-    }
-
-    public String getLogFormat() {
-        return logFormat;
-    }
 }
